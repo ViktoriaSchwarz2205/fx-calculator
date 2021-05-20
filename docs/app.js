9d84e1568d50bc777a2153a0263739c11b7eb283
Viktoria Schwarz <2010738629@stud.fh-kufstein.ac.at>
2021-05-20 18:40:01 +0200

    finished homework

--- docs/app.js
+++ docs/app.js
@@ -7,3 +7,41 @@ document.querySelectorAll('select').forEach(element => {
         <option value="BTC">BTC</option>
     `;
 });
+
+const fixerUri = "fixer.json";
+
+async function convert(inputValue, inputCurrency, outputCurrency) {
+    const response = await fetch(fixerUri);
+    const data = await response.json();
+    const rates = data['rates'];
+    rates["EUR"] = 1.;
+    return inputValue / rates[inputCurrency] * rates[outputCurrency];
+}
+
+document.querySelector('button').addEventListener('click', async () => {        
+    const inputCurrency = document.querySelector('[name="input-currency"]').value;
+    const outputCurrency = document.querySelector('[name="output-currency"]').value;    
+    const inputValue = document.querySelector('[name="input-value"]').value;
+    const outputValue = await convert(inputValue, inputCurrency, outputCurrency);
+    document.querySelector('[name="output-value"]').value = Math.round(outputValue, 2);            
+});
+
+let pwaPrompt;
+const prompt = document.querySelector('#pwa-install-prompt');
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    pwaPrompt = e;
+    prompt.classList.add('show');
+})
+
+prompt.addEventListener('click', function(event) {
+    if (event.target.dataset.id === 'pwa-install-y' && pwaPrompt) {
+        pwaPrompt.prompt();
+        pwaPrompt.userChoice.then(() => {
+            prompt.classList.remove('show');
+            pwaPrompt = null;
+        });
+    } else {
+        prompt.classList.remove('show');
+    }
+});
